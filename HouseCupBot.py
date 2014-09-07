4d404cba46b4cf779431524f45027450b032723f
feenahm <websterusaf@gmail.com>
2014-09-07 15:44:00 -0600

    Added more comments!

--- HouseCupBot.py
+++ HouseCupBot.py
@@ -10,7 +10,7 @@ Version: 1.1'''
 BOTNAME		= 'HouseCupBot'
 PASSWORD	= getpass('Password: ')
 USERAGENT	= 'HouseCupBot keeps a running score for Hogwarts houses. Author: u/D_Web'
-SUBREDDIT 	= 'all'
+SUBREDDIT 	= 'requestabot+all'
 
 #----------Replies/Comment Parsing Configuration----------#
 HOUSES		= ['gryffindor','hufflepuff','ravenclaw','slytherin']
@@ -42,7 +42,7 @@ cur = sql.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS oldposts(ID TEXT)')
 cur.execute('CREATE TABLE IF NOT EXISTS submissions(NAME TEXT, HOUSE TEXT, POINTS INTEGER, DATE TIMESTAMP)')
 cur.execute('CREATE TABLE IF NOT EXISTS scores(NAME TEXT, POINTS INTEGER)')
-cur.execute('CREATE TABLE IF NOT EXISTS winners(NAME TEXT, TIME_PER TEXT, POINTS INTEGER)')
+cur.execute('CREATE TABLE IF NOT EXISTS winners(NAME TEXT, TIME_PER TEXT, POINTS INTEGER)')	
 sql.commit()
 print 'DONE'
 
@@ -56,7 +56,7 @@ print 'DONE'
 def RateCheck(redditor):
 	cur.execute('SELECT date FROM submissions WHERE NAME=?', (redditor,))						# Get the last time the user submitted points for a house
 	try:	
-		lastPost = datetime.strptime(str(cur.fetchone()[0]), '%Y-%m-%d %H:%M:%S')				# Convert the timestamp to datetime object
+		lastPost = datetime.strptime(str(cur.fetchone()[0]), '%Y-%m-%d %H:%M:%S')				# Convert the timestamp to datetime object so we can compare dates
 		timeDelta = (datetime.now() - lastPost).seconds								# Find the delta between now and the last post time
 		if timeDelta < 1800:											# If it has been less than 30 minutes since the last post, ignore the post (return False)
 			print '%s attempted to post again after %im%is' % (redditor, time_delta/60, time_delta%60)
@@ -71,9 +71,9 @@ def PostReply(post, points, awardedHouse):
 	pointsList = []
 	for house in HOUSES:
 		try:
-			housePoints = int(cur.execute('SELECT points FROM scores WHERE NAME=?', (house,)).fetchone()[0])
+			housePoints = int(cur.execute('SELECT points FROM scores WHERE NAME=?', (house,)).fetchone()[0])	# Try to get the points that a house has
 		except:
-			housePoints = 0
+			housePoints = 0												# If the previous code errors out that means they dont have any points
 		pointsList.append(housePoints)
 	post.reply(RESPONSE % (points, awardedHouse.title(), pointsList[0], pointsList[1], pointsList[2], pointsList[3]))
 
@@ -82,9 +82,9 @@ def SubScan():
 	sub = r.get_subreddit(SUBREDDIT)
 	posts = sub.get_comments(limit=POSTLIMIT)
 	for p in posts:																	
-		pID = p.id
+		pID = p.id									# Grab the post ID to insert it into the oldposts table so the bost doesn't try and process that comment again later
 		try:
-			pAuth = p.author.name
+			pAuth = p.author.name							# Get the poster's username to make sure it was not the bot that commented and so we can track that user's post time, points, and house
 		except:
 			pAuth = '[DELETED]'
 		cur.execute('SELECT * FROM oldposts WHERE ID=?', (pID,))
@@ -98,12 +98,12 @@ def SubScan():
 				house = regCheck.group(3)
 				if newPoints >= POINTMIN and newPoints <= POINTMAX and type(newPoints) is int:
 					try:
-						currentPoints = int(cur.execute('SELECT points FROM scores WHERE NAME=?', (house,)).fetchone()[0])
-						updatedPoints = currentPoints + newPoints
+						currentPoints = int(cur.execute('SELECT points FROM scores WHERE NAME=?', (house,)).fetchone()[0])			# Get the house's current points
+						updatedPoints = currentPoints + newPoints										# Add the new points and the current points so that we can update the 'points' value in the 'scores' table for that house
 						cur.execute('UPDATE scores SET points=? WHERE NAME=?', (updatedPoints, house))
 					except:
-						cur.execute('INSERT INTO scores VALUES(?,?)', (house, newPoints))
-					cur.execute('INSERT INTO submissions VALUES(?,?,?,?)', (pAuth, house, newPoints, datetime.now().replace(microsecond=0)))
+						cur.execute('INSERT INTO scores VALUES(?,?)', (house, newPoints))							# If the house does not currently have any points, just set the point value for that house to 'newPoints'
+					cur.execute('INSERT INTO submissions VALUES(?,?,?,?)', (pAuth, house, newPoints, datetime.now().replace(microsecond=0)))	# Add the redditors information to the 'submissions' table
 					sql.commit()
 					print 'Attempting to reply to %s: "%s"...' % (pAuth, regCheck.group(0)),
 					try: 
